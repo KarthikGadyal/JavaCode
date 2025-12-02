789d0e6b35a39bc0fc54a72f232f0ec668c56e90
KarthikGadyal <154061602+KarthikGadyal@users.noreply.github.com>
2025-12-02 17:35:08 +0530

    emove spaces

--- src/com/practice/pattern/demo2.java
+++ src/com/practice/pattern/demo2.java
@@ -6,9 +6,6 @@ public class demo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	
-		
 		for(int i = 1; i <= 5; i++) {
 			for(int j = 1; j <= 5; j++) {
 				if(i==1 || j== 1 || i == 5 || j == 5 || (i==3 && j== 3)){
