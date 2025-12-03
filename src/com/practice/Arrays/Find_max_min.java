185fa70709ae75516444456094952fe17c28afc9
KarthikGadyal <KARTHIK.20201CSE0595@presidencyuniversity.in>
2025-12-03 11:36:47 +0530

    update arrays max and min

--- src/com/practice/Arrays/Find_max_min.java
+++ src/com/practice/Arrays/Find_max_min.java
@@ -3,7 +3,7 @@ package com.practice.Arrays;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class MaxArray {
+public class Find_max_min {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,17 +16,36 @@ public class MaxArray {
 			arr[i] = scan.nextInt();
 		}
 		
+		System.out.println(Arrays.toString(arr));
+		
+		Logic fm = new Logic();
+		System.out.println(fm.findMax(arr));
+		System.out.println(fm.findMin(arr));
+				
+	}
+
+}
+
+class Logic{
+	public String findMax(int arr[]) {
 		int max = arr[0];
 		for(int i = 1; i < arr.length; i++) {
 			if(max < arr[i]) {
 				max = arr[i];
 			}
 		}
-		
-		
-		System.out.println(Arrays.toString(arr));
-		System.out.println("Maximum :" + max);
+		return "MAXIMUM : " + max;
 		
 	}
-
+	
+	public String findMin(int arr[]) {
+		int min = arr[0];
+		for(int i = 1; i < arr.length; i++) {
+			if(min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		return "Minimum : " + min;
+		
+	}
 }
