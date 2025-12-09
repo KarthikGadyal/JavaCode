13f96045cc8765ada0f59e7ee23158bced50c62d
KarthikGadyal <KARTHIK.20201CSE0595@presidencyuniversity.in>
2025-12-09 11:33:22 +0530

    modified pattern demo2

--- src/com/practice/pattern/demo2.java
+++ src/com/practice/pattern/demo2.java
@@ -1,38 +1,22 @@
 package com.practice.pattern;
 
-import java.util.Scanner;
 
-public class demo2 {
 
-<<<<<<< HEAD
+public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i = 1; i <= 5; i++) {
-			for(int j = 1; j <= 5; j++) {
-				if(i==1 || j== 1 || i == 5 || j == 5 || (i==3 && j== 3)){
-				System.out.print("* ");
-				}else {
-					System.out.print("  ");
-				}
-			}
-=======
-			public static void main(String[] args) {
-				// TODO Auto-generated method stub
+		  // First box
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || j == 1 || i == 5 || j == 5 || (i == 3 && j == 3)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();   // <-- Missing earlier
+        }
 
->>>>>>> 747c640 (new package Arrays)
-			
-				for(int i = 1; i <= 5; i++) {
-					for(int j = 1; j <= 5; j++) {
-						if(i==1 || j== 1 || i == 5 || j == 5 || (i==3 && j== 3)){
-						System.out.print("* ");
-						}else {
-							System.out.print("  ");
-						}
-					}
-					
-					System.out.println();
-				}
-				
-			}
-
+        System.out.println();
+	}
 }
